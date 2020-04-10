013ed75eba55d2ec49b8595bff4b737525172d57
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2020-04-09 22:09:25 -0300

    11th class. Binding and screen size.

---
+++ Design+Code/Design+Code/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  Design+Code
+//
+//  Created by João Medeiros on 09/04/20.
+//  Copyright © 2020 Joao Medeiros. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
